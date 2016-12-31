b77fc4cc0cc77a0b88d25ba9c5ea198ea594b58a
Anthony FAVIER <anthony.favier03@gmail.com>
2017-01-01 00:29:08 +0100

    header_ok

--- script/style.js
+++ script/style.js
@@ -1,1 +1,15 @@
-$( ".menu" ).hide();
+function menuHamburger(){
+	document.getElementById("dropDown").classList.toggle("show");
+}
+window.onclick = function(event){
+	if (!event.target.matches('.hamburger')) {
+		var dropdowns = document.getElementByClassName("menu");
+		var i;
+		for (i=0; i<dropdowns.length; i++){
+			var openDropdown = dropdowns[i];
+			if (openDropdown.classList.contains("show")){
+				openDropdown.classList.remove("show");
+			}
+		}
+	}
+}
